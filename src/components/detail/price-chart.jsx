53309437e19bddec63fb1d0ccd870b6403485734
ualperen41 <alperenb.200021@gmail.com>
2025-09-28 15:50:30 +0300

    düzen

--- src/components/detail/price-chart.jsx
+++ src/components/detail/price-chart.jsx
@@ -3,17 +3,16 @@ import "chart.js/auto";
 
 const PriceChart = ({ symbol, days, priceHistory }) => {
   const data = {
-    labels: priceHistory.map((item) => formatDate(days, item.timestamp)),
+    labels: priceHistory.map((item) => item.timestamp),
     datasets: [
       {
-        label: `${symbol} Fiyat`,
-        data: priceHistory.map((item) => item.price),
+        label: "Dataset 1",
+        data: [100, 200, 400, 250, 90, 899, 356, 100],
         borderColor: "rgb(255,99,132)",
         backgroundColor: "rgba(255,99,132,0.5)",
       },
     ],
   };
-
   return (
     <div className="h-80 w-full">
       <Line data={data} />
